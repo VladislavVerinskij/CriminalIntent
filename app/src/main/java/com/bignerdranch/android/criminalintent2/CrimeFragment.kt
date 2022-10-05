095d1de47c62d38ceccbcb05afa4189388a8dc7b
Vladislav <vladislav.verinskij@gmail.com>
2022-10-05 20:58:50 +0300

    database

--- app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeFragment.kt
@@ -16,6 +16,7 @@ import java.util.*
 
 private const val ARG_CRIME_ID = "crime_id"
 private const val TAG = "CrimeFragment"
+private const val DIALOG_DATE = "DialogDate"
 
 
 class CrimeFragment: Fragment() {
@@ -48,10 +49,6 @@ class CrimeFragment: Fragment() {
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
 
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
 
         return view
     }
@@ -99,6 +96,12 @@ class CrimeFragment: Fragment() {
         solvedCheckBox.apply {
             setOnCheckedChangeListener {_, isChecked ->
                 crime.isSolved = isChecked
+            }
+        }
+
+        dateButton.setOnClickListener {
+            DatePickerFragment().apply {
+                show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
             }
         }
     }

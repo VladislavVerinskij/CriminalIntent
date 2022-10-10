dbd8c99a15f827134eb964e9e4168da7d9f196df
Vladislav <vladislav.verinskij@gmail.com>
2022-10-10 12:49:45 +0300

    add button in menu

--- app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeListViewModel.kt
@@ -7,4 +7,8 @@ class CrimeListViewModel : ViewModel() {
 
     private val crimeRepository = CrimeRepository.get()
     val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime){
+        crimeRepository.addCrime(crime)
+    }
 }
